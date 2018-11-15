662e54f037f787b6c8bb659a65de8eda35d879fb
Tulsi Prasad Jaiswal <tulsithegreat98@gmail.com>
2018-11-16 02:46:40 +0530

    Update index(WithDistinctUser&Brand).php

--- index(WithDistinctUser&Brand).php
+++ index(WithDistinctUser&Brand).php
@@ -1,7 +1,13 @@
 <?
+
+	
+	$hashtag="#baseball"; 
+	//Store hashtag in variable
+		
 	require_once('include/twitteroauth.php');
 	require_once('include/OAuth.php');
 	require_once('include/TwitterAPIExchange.php');
+	//Importing libraries which are stores in Include folder
 
 		$settings = array(
 		    'oauth_access_token' => '1063029996011155456-CcmPREIoYLqce9FwvpLWkLWAEiWQ4s',
@@ -9,103 +15,37 @@
 		    'consumer_key' => '2Iv3usoHNT60gopvZAwGELKvf',
 		    'consumer_secret' => 'zIy7YOpKaFOfiCJ5NnZex5ziXFe5xKMIJQ7A8iHzOeORwcPH2q'
 				);
+		//Authentication
 
 	$url = 'https://api.twitter.com/1.1/search/tweets.json';
+	//Reference for above url = Twitter API Documentation
 	$requestMethod = 'GET';
-	$getfield = '?q=#Brand&result_type=recent&count=200';
+	
+	
+	$getfield = '?q='.$hashtag.'&result_type=recent&count=100';
+	//To make query
 	$twitter = new TwitterAPIExchange($settings);
 	$string2 = json_decode($twitter->setGetfield($getfield)
 	             ->buildOauth($url, $requestMethod)
         	     ->performRequest(),$assoc = TRUE);
+	//Performing request and decoding in JSON 
 
 
-	$brands_handle=array("NULL");
-	array_pop($brands_handle);
+	
+	echo "<br/>";
+	echo "Twitter Handle"."<br/>";
 
-	$users_handle=array("NULL");
-	array_pop($users_handle);
-
-	$threshold=10;
+	$count=1;// To provide 10 recent twitter_handle & hence to keep count in variable
 	foreach($string2['statuses'] as $items)
 	{
-	
-		
-		$sum=0;
-		$follower_count=$items['user']['followers_count'];
-		if($follower_count>3000)
-		{
-			$sum=$sum+5;
-		}
-		$friends_count=$items['user']['friends_count'];
-		if($friends_count!=0&& $follower_count/$friends_count>3)
-		{
-			$sum=$sum+5;
-		}
-		$verified=$items['user']['verified'];
-		if($verified==true)
-		{
-			$sum=$sum+10;
-		}
-		$is_default_profile=$items['user']['default_profile_image'];
-		
-		$bio = $items['user']['description'];
-		if ((strpos($bio, 'official') !== false)&&(strpos($bio, 'Official') !== false)) 
-  		{
-		$sum=$sum+10;
-
-		}else if((strpos($bio, 'Official handle') !== false)&&(strpos($bio, 'Official handle') !== false))
-		{
-
-
-		$sum=$sum+15;
-
-		}else if(strpos($bio, 'brand') !== false && strpos($bio, 'company') !== false && strpos($bio, 'Brand') !== false)
-		{
-
-
-		$sum=$sum+20;
-		}else if(strpos($bio, 'user') !== false&&(strpos($bio, 'personal') !== false))
-		{
-			$sum-=20;
-		}
-
-
-		if($is_default_profile==true)
-		{
-			array_push($users_handle,$items['user']['screen_name']);
-		}else
-		{
-			if($sum>=$threshold)
-			{
-				array_push($brands_handle,$items['user']['screen_name']);
-			}else
-			{
-				array_push($users_handle,$items['user']['screen_name']);
-			}
-		}
-	}
-	echo "Users"."<br/>";
-	$c=1;
-	foreach($users_handle as $items)
-	{
-		if($c>5)
+		if($count>10)
 		{
 		break;
 		}
-		echo "@".$items."<br/>";
+		echo "@".$items['user']['screen_name']."<br/>";
+		//Screen name is basically Twitter Handle.
 		
-		$c=$c+1;
+		$count=$count+1;
+		
 	}
-	echo "<br/>";
-	echo "Brands"."<br/>";
-	$c=1;
-	foreach($brands_handle as $items)
-	{
-		if($c>5)
-		{
-		break;
-		}
-		echo "@".$items."<br/>";
-	}
-
 ?>
